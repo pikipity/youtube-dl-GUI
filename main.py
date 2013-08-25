b2c7bf618e6d2d4f2fa40091e613ddb7d00f99d6
pikipity <wangze19910407@gmail.com>
2013-08-25 14:23:55 +0800

    Add Task arrangment
    Add Task arragement

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import subprocess
 import time
 import os
 
+k=1
 ProgramPath=sys.argv[0][0:sys.argv[0].rfind('/')]#no final "/"
 DownloadProgram=ProgramPath+'/youtube-dl'
 DownloadPathDefault=ProgramPath+'/Downloads/'
@@ -46,7 +47,7 @@ Options=["--playlist-start NUMBER    playlist video to start at (default is 1)",
          "--get-format               simulate, quiet but print output format",
          "--sub-lang LANG            language of the subtitles to download (optional) use IETF language tags like 'en'",
          "--audio-quality QUALITY    ffmpeg/avconv audio quality specification, inser a value between 0 (better) and 9 (worse) for VBR or a specific bitrate like 128K (default 5)"]
-#DoenloadArrayLock=threading.Lock()
+DownloadArrayLock=threading.Lock()
 ###################################################################
 #选择下载路径
 def ChooseDownloadPath():
@@ -68,47 +69,177 @@ def PasteURL():
     DownloadURLEntry.insert("end",URL)
 ##################################################################
 #下载任务
-#class DownloadTask(threading.Thread):
-#    def __init__(self):
-#        threading.Thread.__init__(self)
+class DownloadTask(threading.Thread):
+    def __init__(self,DownloadProgram,DownloadURL,DownloadPath,DownloadOptions):
+        threading.Thread.__init__(self)
+        #定义变量
+        self.Program=DownloadProgram
+        self.name='Waiting'
+        self.speed="--"
+        self.path=DownloadPath
+        self.process="0%"
+        self.size="--"
+        self.ETA="--"
+        self.URL=DownloadURL
+        self.Options=DownloadOptions
+        self.position=DownloadArray.size()
+        #打印信息
+        DownloadArrayLock.acquire()
+        Info="Name: %s  Speed: %s  %s of %s ETA: %s  Path: %s"%(self.name,self.speed,self.process,self.size,self.ETA,self.path)
+        DownloadArray.insert(self.position,Info)
+        DownloadArrayLock.release()
+        if self.URL[0:4]!="http" and self.URL[0:5]!="https":
+            tkMessageBox.showwarning("Wrong URL","%s is not a correct URL (include http or https)" \
+                                                 %self.URL)
+            DownloadArrayLock.acquire()
+            DownloadArray.delete(self.position)
+            DownloadArrayLock.release()
+        else:
+            P=subprocess.Popen("%s %s -e -s"%(self.Program,self.URL), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            self.name=P.stdout.readline()[0:P.stdout.readline().find('\n')]
+    def run(self):
+        Error=0
+        TaskWrong=0
+        for Num in range(0,DownloadArray.size()-1):
+            while DownloadArray.get(Num)[DownloadArray.get(Num).find('Name:')+6:DownloadArray.get(Num).find('Speed:')-2]=="Waiting":
+                if k==0:
+                    break
+                else:
+                    pass
+            while self.name=='Waiting':
+                if k==0:
+                    break
+                else:
+                    pass
+            if self.name==DownloadArray.get(Num)[DownloadArray.get(Num).find('Name:')+6:DownloadArray.get(Num).find('Speed:')-2]:
+                TaskWrong=1
+        if self.URL[0:4]!="http" and self.URL[0:5]!="https":
+            pass
+        elif k==0:
+            pass
+        elif TaskWrong:
+            tkMessageBox.showwarning("Warning","The task has been existed.")
+            DownloadArray.delete(self.position)
+        else:
+            P=subprocess.Popen("%s --newline %s %s -o '%s"%(self.Program,self.Options,self.URL,self.path)+"%(title)s.%(ext)s'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            tmp=P.stdout.readline()
+            while tmp!='':
+                if tmp.find('Error')>=0 or tmp.find('error')>=0 or tmp.find('ERROR')>=0:
+                    DownloadArrayLock.acquire()
+                    try:
+                        choosing=int(DownloadArray.curselection()[0])
+                    except:
+                        choosing=-1
+                    DownloadArray.delete(self.position)
+                    if self.position!=0:
+                        if DownloadArray.get(self.position-1)=='':
+                            if choosing>=self.position:
+                                choosing=choosing-1
+                            self.position=self.position-1
+                    Info="%s: Error"%(self.name)
+                    DownloadArray.insert(self.position,Info)
+                    DownloadArray.selection_set(choosing)
+                    DownloadInfo.insert(1.0,'%s: Error. Please double check it\n'%self.name)
+                    DownloadArrayLock.release()
+                    Error=1
+                elif tmp.find('ETA')>0:
+                    self.speed=tmp[tmp.find('at')+4:tmp.find('ETA')-1]
+                    self.process=tmp[tmp.find(']')+1:tmp.find('of')-1]
+                    self.size=tmp[tmp.find('of')+3:tmp.find('at')-1]
+                    self.ETA=tmp[tmp.find('ETA')+4:len(tmp)]
+                    DownloadArrayLock.acquire()
+                    try:
+                        choosing=int(DownloadArray.curselection()[0])
+                    except:
+                        choosing=-1
+                    DownloadArray.delete(self.position)
+                    if self.position!=0:
+                        if DownloadArray.get(self.position-1)=='':
+                            if choosing>=self.position:
+                                choosing=choosing-1
+                            self.position=self.position-1
+                    Info="Name: %s  Speed: %s  %s of %s ETA: %s  Path: %s"%(self.name,self.speed,self.process,self.size,self.ETA,self.path)
+                    DownloadArray.insert(self.position,Info)
+                    DownloadArray.selection_set(choosing)
+                    DownloadInfo.insert(1.0,"%s: %s\n"%(self.name,tmp))
+                    DownloadArrayLock.release()
+                tmp=P.stdout.readline()
+            if Error:
+                tmp=DownloadArray.get(self.position)
+                while k:
+                    DownloadArrayLock.acquire()
+                    if tmp==DownloadArray.get(self.position):
+                        if self.position!=0:
+                            if DownloadArray.get(self.position-1)=='':
+                                self.position=self.position-1
+                                DownloadArray.delete(self.position+1)
+                                DownloadArray.insert(self.position,tmp)
+                        DownloadArrayLock.release()
+                        time.sleep(3)
+                    else:
+                        DownloadArrayLock.release()
+                        break
+            else:
+                DownloadArrayLock.acquire()
+                DownloadArray.delete(self.position)
+                if self.position!=0:
+                    if DownloadArray.get(self.position-1)=='':
+                        self.position=self.position-1
+                Info="%s: finish!!"%(self.name)
+                DownloadArray.insert(self.position,Info)
+                DownloadInfo.insert(1.0,'%s: finish!!\n'%self.name)
+                DownloadArrayLock.release()
+                tmp=DownloadArray.get(self.position)
+                while k:
+                    DownloadArrayLock.acquire()
+                    if tmp==DownloadArray.get(self.position):
+                        DownloadArray.delete(self.position)
+                        if self.position!=0:
+                            if DownloadArray.get(self.position-1)=='':
+                                self.position=self.position-1
+                                DownloadArray.delete(self.position+1)
+                                DownloadArray.insert(self.position,tmp)
+                        DownloadArrayLock.release()
+                        time.sleep(3)
+                    else:
+                        DownloadArrayLock.release()
+                        break
 ###################################################################
 #Download
-def DownloadProcess(DownloadURL,DownloadPath,DownloadOptions):
-    if DownloadURL[0:4]!="http" and DownloadURL[0:5]!="https":
-        tkMessageBox.showwarning("Wrong URL","%s is not a correct URL (include http or https)" \
-                                             %DownloadURL)
-    else:
-        P=subprocess.Popen("%s --newline %s %s -o '%s"%(DownloadProgram,DownloadOptions,DownloadURL,DownloadPath)+"%(title)s.%(ext)s'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        DownloadInfo.delete(1.0,"end")
-        DownloadButton['state']='disable'
-        tmp=P.stdout.readline()
-        while tmp!='':
-            DownloadInfo.insert(1.0,"%s\n"%tmp)
-            if P.stdout.readline().find("Error")>=0 or P.stdout.readline().find("error")>=0 or P.stdout.readline().find("ERROR")>=0:
-                tkMessageBox.showwarning("Downlad Fail","There is something wrong. Please double check inputs.")
-                break
-            else:
-                tmp=P.stdout.readline()
-        DownloadInfo.insert(1.0,"%s\n"%"Finish!!")
-        DownloadButton['state']='normal'
+#def DownloadProcess(DownloadURL,DownloadPath,DownloadOptions):
+#    if DownloadURL[0:4]!="http" and DownloadURL[0:5]!="https":
+#        tkMessageBox.showwarning("Wrong URL","%s is not a correct URL (include http or https)" \
+#                                             %DownloadURL)
+#    else:
+#        P=subprocess.Popen("%s --newline %s %s -o '%s"%(DownloadProgram,DownloadOptions,DownloadURL,DownloadPath)+"%(title)s.%(ext)s'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#        DownloadInfo.delete(1.0,"end")
+#        tmp=P.stdout.readline()
+#        while tmp!='':
+#            DownloadInfo.insert(1.0,"%s\n"%tmp)
+#            if P.stdout.readline().find("Error")>=0 or P.stdout.readline().find("error")>=0 or P.stdout.readline().find("ERROR")>=0:
+#                tkMessageBox.showwarning("Downlad Fail","There is something wrong. Please double check inputs.")
+#                break
+#            else:
+#                tmp=P.stdout.readline()
+#        DownloadInfo.insert(1.0,"%s\n"%"Finish!!")
 #开始下载
 def BeginDownload():
+    DownloadButton['state']='disable'
     DownloadPath=DownloadPathEntry.get()
     DownloadURL=DownloadURLEntry.get()
     DownloadOptions=OptionsEntry.get()
     if os.path.exists(DownloadPath):
-        t=threading.Thread(target=DownloadProcess,args=(DownloadURL,DownloadPath,DownloadOptions))
-        t.start()
+        DownloadTask(DownloadProgram,DownloadURL,DownloadPath,DownloadOptions).start()
     else:
         if tkMessageBox.askquestion("Wrong Path",\
                                  "There isn't %s. Would you like to create it?"%DownloadPath)=='yes':
             try:
                 os.makedirs(DownloadPath)
-                t=threading.Thread(target=DownloadProcess,args=(DownloadURL,DownloadPath,DownloadOptions))
-                t.start()
+                DownloadTask(DownloadProgram,DownloadURL,DownloadPath,DownloadOptions).start()
             except:
                 tkMessageBox.showwarning("Wrong Path","%s is not a path"\
                     %DownloadPath)
+    DownloadButton['state']='normal'
 ###################################################################
 #打开文件夹
 def OpenDir():
@@ -299,6 +430,35 @@ def ChooseOptionsOK():
             OptionsEntry.insert('end',Options[int(i)][0:Options[int(i)].find("    ")+1])
     ChooseOptionsWindow.destroy()
 ########################################################################
+#关闭按钮
+def callback(event=None):
+    global k
+    k=0
+    DownloadArrayLock.acquire()
+    DownloadInfo.insert(1.0,"I am closing.......\n")
+    DownloadArrayLock.release()
+    root.quit()
+#######################################################################
+#Delete Task
+def DeleteTask():
+    if DownloadArray.curselection()==():
+        tkMessageBox.showwarning('Warning',"Please choose a download task")
+    else:
+        DownloadArrayLock.acquire()
+        tmp=DownloadArray.get(DownloadArray.curselection()[0])
+        DownloadArrayLock.release()
+        if tmp.find('Error')>=0 or tmp.find('finish')>=0:
+            DownloadArrayLock.acquire()
+            choosing=DownloadArray.curselection()[0]
+            DownloadArray.delete(choosing)
+            if int(choosing)!=DownloadArray.size():
+                DownloadArray.selection_set(choosing)
+            else:
+                DownloadArray.selection_set(int(choosing)-1)
+            DownloadArrayLock.release()
+        else:
+            tkMessageBox.showwarning('Warning','This task has not been finished. Please waiting.')
+#######################################################################
 
 
 
@@ -306,7 +466,7 @@ def ChooseOptionsOK():
 root=Tkinter.Tk()
 
 ControlLabelFrame=Tkinter.LabelFrame(root,text="Download Information")
-ControlLabelFrame.pack(side="left")
+ControlLabelFrame.pack(side="left",fill="y")
 
 DownloadPathFrame=Tkinter.Frame(ControlLabelFrame)
 DownloadPathFrame.pack()
@@ -352,17 +512,19 @@ sl.pack(side="left",fill="y")
 DownloadInfo['yscrollcommand']=sl.set
 sl['command']=DownloadInfo.yview
 
-#DownloadTaskLabelFrame=Tkinter.LabelFrame(root,text="Download Task")
-#DownloadTaskLabelFrame.pack(side="left")
-#DeletTaskButton=Tkinter.Button(DownloadTaskLabelFrame,text="Delete Task")
-#DeletTaskButton.pack()
-#TaskFrame=Tkinter.Frame(DownloadTaskLabelFrame)
-#TaskFrame.pack()
-#DownloadArray=Tkinter.Listbox(TaskFrame,width=42,height=26)
-#DownloadArray.pack(side="left")
-#Tasksl=Tkinter.Scrollbar(TaskFrame)
-#Tasksl.pack(side="left",fill="y")
-#DownloadArray['yscrollcommand'] = Tasksl.set
-#Tasksl['command'] = DownloadArray.yview
+DownloadTaskLabelFrame=Tkinter.LabelFrame(root,text="Download Task",height=100)
+DownloadTaskLabelFrame.pack(side="left",fill='y')
+DeletTaskButton=Tkinter.Button(DownloadTaskLabelFrame,text="Delete Task",command=DeleteTask)
+DeletTaskButton.pack()
+TaskFrame=Tkinter.Frame(DownloadTaskLabelFrame)
+TaskFrame.pack()
+DownloadArray=Tkinter.Listbox(TaskFrame,width=42,height=26)
+DownloadArray.pack(side="left")
+Tasksl=Tkinter.Scrollbar(TaskFrame)
+Tasksl.pack(side="left",fill="y")
+DownloadArray['yscrollcommand'] = Tasksl.set
+Tasksl['command'] = DownloadArray.yview
+
+root.protocol( 'WM_DELETE_WINDOW', callback)
 
 root.mainloop()
